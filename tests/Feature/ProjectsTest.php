c3e35c075a3a8a923611ae2b47ee18ac9d4d19b2
Dani Ihza Farrosi <dani.videda@gmail.com>
2020-05-29 10:38:22 +0700

    Add actingAs Authenticated users

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -11,8 +11,20 @@ class ProjectsTest extends TestCase
     use WithFaker, RefreshDatabase;
 
     /** @test */
+    public function only_authenticated_users_can_create_project()
+    {
+        // $this->withoutExceptionHandling();
+
+        $attributes = factory('App\Project')->raw();
+
+        $this->post('/projects', $attributes)->assertRedirect('login');
+    }
+
+    /** @test */
     public function a_user_can_create_a_project()
     {
+        $this->actingAs(factory('App\User')->create());
+
         $this->withoutExceptionHandling();
 
         $attributes = [
@@ -42,6 +54,8 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_project_requires_a_title()
     {
+        $this->actingAs(factory('App\User')->create());
+
         $attributes = factory('App\Project')->raw(['title' => '']);
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('title');
@@ -50,18 +64,10 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_project_requires_a_description()
     {
+        $this->actingAs(factory('App\User')->create());
+
         $attributes = factory('App\Project')->raw(['description' => '']);
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('description');
-    }
-    
-    /** @test */
-    public function a_project_requires_an_owner()
-    {
-        $this->withoutExceptionHandling();
-
-        $attributes = factory('App\Project')->raw();
-
-        $this->post('/projects', $attributes)->assertRedirect('login');
     }
 }
