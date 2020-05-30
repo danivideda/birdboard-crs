ae53f8be8cdd8de6cd4c3c7b079f543839b9928d
Dani Ihza Farrosi <dani.videda@gmail.com>
2020-05-30 16:43:59 +0700

    Task UI Update

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -41,9 +41,9 @@ class ProjectsController extends Controller
 
         // $attributes['owner_id'] = auth()->id();
 
-        auth()->user()->projects()->create($attributes);
+        $project = auth()->user()->projects()->create($attributes);
 
         // redirect
-        return redirect('/projects');
+        return redirect($project->path());
     }
 }
--- tests/Feature/ProjectTasksTest.php
+++ tests/Feature/ProjectTasksTest.php
@@ -12,6 +12,37 @@ class ProjectTasksTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
+    public function guests_cannot_add_tasks_to_projects()
+    {
+       $project = factory('App\Project')->create();
+
+       $this->post($project->path() . '/tasks')->assertRedirect('login');
+    }
+
+
+    /** @test */
+    public function only_the_owner_of_a_project_may_add_tasks()
+    {
+        $this->signIn();
+        $project = factory('App\Project')->create();
+        $this->post($project->path() . '/tasks', ['body' => 'Test task'])
+            ->assertStatus(403);
+        $this->assertDatabaseMissing('tasks', ['body' => 'Test task']);
+    }
+
+    /** @test */
+    public function only_the_owner_of_a_project_may_update_a_task()
+    {
+        $this->signIn();
+        $project = factory('App\Project')->create();
+        $task = $project->addTask('test task');
+
+        $this->patch($task->path(), ['body' => 'changed'])
+            ->assertStatus(403);
+        $this->assertDatabaseMissing('tasks', ['body' => 'changed']);
+    }
+
+    /** @test */
     public function a_project_can_have_tasks()
     {   
         $this->signIn();
@@ -20,11 +51,35 @@ class ProjectTasksTest extends TestCase
             factory(Project::class)->raw()
         );
 
-        $this->post($project->path() . '/tasks', ['body' => 'Test Task']);
+        $this->post($project->path() . '/tasks', ['body' => 'Test task']);
 
         $this->get($project->path())
-            ->assertSee('Test Task');
+            ->assertSee('Test task');
 
+    }
+
+    /** @test */
+    public function a_task_can_be_updated()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->signIn();
+
+        $project = auth()->user()->projects()->create(
+            factory(Project::class)->raw()
+        );
+
+        $task = $project->addTask('test task');
+
+        $this->patch($project->path() . '/tasks/' . $task->id, [
+            'body' => 'changed',
+            'completed' => true
+        ]);
+
+        $this->assertDatabaseHas('tasks', [
+            'body' => 'changed',
+            'completed' => true
+        ]);
     }
 
     /** @test */
