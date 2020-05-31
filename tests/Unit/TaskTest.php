86452561d5d0d1ace4f909717307132762c787fe
Dani Ihza Farrosi <dani.videda@gmail.com>
2020-05-31 10:22:28 +0700

    Activity Feeds 2
    Add recordActivity and refactoring it

--- tests/Unit/TaskTest.php
+++ tests/Unit/TaskTest.php
@@ -15,16 +15,28 @@ class TaskTest extends TestCase
     /** @test */
     public function it_belongs_to_a_project()
     {
-       $task = factory(Task::class)->create();
+      $task = factory(Task::class)->create();
 
-       $this->assertInstanceOf(Project::class, $task->project); 
+      $this->assertInstanceOf(Project::class, $task->project); 
     }
 
     /** @test */
     public function it_has_a_path()
     {
+      $task = factory(Task::class)->create();
+
+      $this->assertEquals('/projects/' . $task->project->id . '/tasks/' . $task->id, $task->path());
+    }
+
+    /** @test */
+    public function it_can_be_completed()
+    {
        $task = factory(Task::class)->create();
 
-       $this->assertEquals('/projects/' . $task->project->id . '/tasks/' . $task->id, $task->path());
+       $this->assertFalse($task->completed);
+
+      $task->complete();
+
+      $this->assertTrue($task->fresh()->completed);
     }
 }
