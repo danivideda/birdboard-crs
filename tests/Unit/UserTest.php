d54ecccbcbe7439eae10c208f533fd9e184a7931
Dani Ihza Farrosi <dani.videda@gmail.com>
2020-06-01 12:32:59 +0700

    False positive
    Add accessibleProjects to members

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit;
 
+use App\User;
+use Facades\Tests\Setup\ProjectFactory;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -16,5 +18,26 @@ class UserTest extends TestCase
        $user = factory('App\User')->create();
 
        $this->assertInstanceOf(Collection::class, $user->projects);
+    }
+
+    /** @test */
+    function a_user_has_accessible_project()
+    {
+        $dani = $this->signIn();
+
+        ProjectFactory::ownedBy($dani)->create();
+
+        $this->assertCount(1, $dani->accessibleProjects());
+
+        $tsukihi = factory(User::class)->create();
+        $karen = factory(User::class)->create();
+
+        $project = tap(ProjectFactory::ownedBy($tsukihi)->create())->invite($karen);
+
+        $this->assertCount(1, $dani->accessibleProjects());
+
+        $project->invite($dani);
+
+        $this->assertCount(2, $dani->accessibleProjects());
     }
 }
